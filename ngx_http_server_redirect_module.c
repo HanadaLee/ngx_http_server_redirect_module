32dc115d6abd3b3e32fe831c8517c2041319dd96
Hanada <im@hanada.info>
2024-12-21 00:39:39 +0800

    change: use nginx core function to complete server redirection

--- ngx_http_server_redirect_module.c
+++ ngx_http_server_redirect_module.c
@@ -12,13 +12,12 @@ static void * ngx_http_server_redirect_create_conf(ngx_conf_t *cf);
 static char * ngx_http_server_redirect(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_server_redirect_handler(ngx_http_request_t *r);
 static ngx_int_t ngx_http_server_redirect_post_config(ngx_conf_t *cf);
-static ngx_int_t ngx_http_server_redirect_find_virtual_server(ngx_http_request_t *r, u_char *host, size_t len);
 
 
 static ngx_command_t  ngx_http_server_redirect_commands[] = {
 
     { ngx_string("server_redirect"),
-      NGX_HTTP_SRV_CONF | NGX_CONF_TAKE1,
+      NGX_HTTP_SRV_CONF|NGX_CONF_TAKE1,
       ngx_http_server_redirect,
       NGX_HTTP_SRV_CONF_OFFSET,
       0,
@@ -63,11 +62,11 @@ ngx_module_t  ngx_http_server_redirect_module = {
 static char *
 ngx_http_server_redirect(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    ngx_str_t *args;
+    ngx_str_t *value;
     ngx_http_server_redirect_conf_t *srcf = conf;
 
     args = cf->args->elts;
-    srcf->new_server = args[1];
+    srcf->new_server = value[1];
     
     return NGX_CONF_OK;
 }
@@ -80,11 +79,14 @@ ngx_http_server_redirect_create_conf(ngx_conf_t *cf)
 
     conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_server_redirect_conf_t));
     if (conf == NULL) {
-        return NGX_CONF_ERROR;
+        return NULL;
     }
 
-    conf->new_server.data = NULL;
-    conf->new_server.len = 0;
+    /*
+     * set by ngx_pcalloc():
+     *
+     *     conf->new_server = { 0, NULL };
+     */
 
     return conf;
 }
@@ -111,147 +113,31 @@ ngx_http_server_redirect_post_config(ngx_conf_t *cf)
 }
 
 
-// Function to find the virtual server
-static ngx_int_t
-ngx_http_server_redirect_find_virtual_server(ngx_http_request_t *r,
-    u_char *host, size_t len)
-{
-    ngx_http_core_loc_conf_t  *clcf;
-    ngx_http_core_srv_conf_t  *cscf;
-
-    if (r->virtual_names == NULL) {
-        return NGX_DECLINED;
-    }
-
-    cscf = ngx_hash_find_combined(&r->virtual_names->names,
-                                  ngx_hash_key(host, len), host, len);
-
-    if (cscf) {
-        goto found;
-    }
-
-#if (NGX_PCRE)
-
-    if (len && r->virtual_names->nregex) {
-        ngx_int_t                n;
-        ngx_uint_t               i;
-        ngx_str_t                name;
-        ngx_http_server_name_t  *sn;
-
-        name.len = len;
-        name.data = host;
-
-        sn = r->virtual_names->regex;
-
-        for (i = 0; i < r->virtual_names->nregex; i++) {
-
-            n = ngx_http_regex_exec(r, sn[i].regex, &name);
-
-            if (n == NGX_OK) {
-                cscf = sn[i].server;
-                goto found;
-            }
-
-            if (n == NGX_DECLINED) {
-                continue;
-            }
-
-            return NGX_ERROR;
-        }
-    }
-
-#endif
-
-    return NGX_DECLINED;
-
-found:
-    
-    r->srv_conf = cscf->ctx->srv_conf;
-    r->loc_conf = cscf->ctx->loc_conf;
-
-    clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
-    r->connection->log->file = clcf->error_log->file;
-#if (NGX_SYSLOG)
-    r->connection->log->syslog = clcf->error_log->syslog;
-#endif
-
-    if (!(r->connection->log->log_level & NGX_LOG_DEBUG_CONNECTION)) {
-        r->connection->log->log_level = clcf->error_log->log_level;
-    }
-
-    return NGX_OK;
-}
-
-
 // Callback function to handle the request
 static ngx_int_t
 ngx_http_server_redirect_handler(ngx_http_request_t *r)
 {
-    ngx_int_t res;
-    ngx_str_t *server;
-    ngx_http_server_redirect_conf_t *srcf;
+    ngx_int_t     res;
+    ngx_str_t    *server;
 
-    // Retrieve the server-level configuration
+    ngx_http_server_redirect_conf_t *srcf;
+    ngx_http_core_srv_conf_t        *cscf;
+
     srcf = ngx_http_get_module_srv_conf(r, ngx_http_server_redirect_module);
     server = &srcf->new_server;
-    
-    // Find the virtual server
-    res = ngx_http_server_redirect_find_virtual_server(r,
-        server->data, server->len);
 
-    if (res == NGX_OK) {
-        ngx_connection_t  *c;
-
-        c = r->connection;
-
-        if (r->plain_http) {
-            ngx_log_error(NGX_LOG_INFO, c->log, 0,
-                    "client sent plain HTTP request to HTTPS port");
-            ngx_http_finalize_request(r, NGX_HTTP_TO_HTTPS);
-            return NGX_ERROR;
-        }
-
-        if (c->read->timer_set) {
-            ngx_del_timer(c->read);
-        }
-
-        c->read->handler = ngx_http_server_redirect_request_handler;
-        c->write->handler = ngx_http_server_redirect_request_handler;
-        r->read_event_handler = ngx_http_handler;
-
-        /* handler will be called in 1ms timeout */
-        ngx_add_timer(r->connection->read, 1);
-        r->main->count++;
-        
-        return NGX_DONE;
+    if (server->data == NULL || server->len == 0) {
+        return NGX_DECLINED;
     }
+
+    if (ngx_http_set_virtual_server(r, server) == NGX_ERROR) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "failed to redirect to new virtual server");
+        return NGX_ERROR;
+    }
+
+    ngx_log_error(NGX_LOG_INFO, r->connection->log, 0,
+                  "Virtual server set to %V", server);
 
     return NGX_DECLINED;
-}
-
-
-// Callback function to handle connection events
-void ngx_http_server_redirect_request_handler(ngx_event_t *ev)
-{
-    ngx_connection_t    *c;
-    ngx_http_request_t  *r;
-    ngx_http_log_ctx_t  *ctx;
-
-    c = ev->data;
-    r = c->data;
-
-    ctx = c->log->data;
-    ctx->current_request = r;
-
-    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, c->log, 0,
-                   "http run request: \"%V?%V\"", &r->uri, &r->args);
-
-    if (ev->write) {
-        r->write_event_handler(r);
-
-    } else {
-        r->read_event_handler(r);
-    }
-
-    ngx_http_run_posted_requests(c);
 }
